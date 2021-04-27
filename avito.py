e132f8ce68eae3262af328c01a404a194916bc33
valyankilyan <valentin.kil.2002@gmail.com>
2021-04-27 17:46:37 +0300

    it works

--- avito.py
+++ avito.py
@@ -3,9 +3,22 @@ from os import listdir
 
 folder = 'pages'
 
+def getHtml(path):
+    return open(path).read()
+    
+def getPrices(path):
+    prices = []
+    html = getHtml(path)
+    soup = BeautifulSoup(html, 'html.parser')
+    prices = [int(p['content']) for p in soup.find_all("meta",  itemprop="price") if p['content'].isdigit()]
+    return prices
+
 def main():
     pages = [folder + '/' + p for p in listdir(folder) if p.split('.')[1] == 'html']
-    print(pages)
+    prices = []
+    for p in pages:
+        prices += getPrices(p)
+    print(prices)
 
 if __name__ == '__main__':
     main()
